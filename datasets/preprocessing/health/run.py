555a7a4d81bd85258c362a09bff788a9c82490a5
3podi <riccardo1.tripodi@mail.polimi.it>
2025-06-10 16:37:13 +0200

    preprocessing health aphp

--- datasets/preprocessing/health/run.py
+++ datasets/preprocessing/health/run.py
@@ -4,10 +4,12 @@ from typing import List
 
 import pandas as pd
 from datasets import load_dataset
-from quickumls import QuickUMLS
+from preprocessing.keywords_extraction import KeywordsExtractor as Matcher
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM
 from vllm import LLM, SamplingParams
+
+import pandas as pd
 
 SAMPLE_SIZE = 1500
 RANDOM_SEED = 42
@@ -24,24 +26,38 @@ PROMPT_PATH = "datasets/preprocessing/health/prompt.txt"
 tqdm.pandas()
 
 
+#class KeywordExtractor:
+#    def __init__(self):
+#        self.matcher = QuickUMLS(quickumls_fp=os.getenv("QUICKUMLS_PATH"))
+#
+#    def extract_keywords(self, text: str) -> str:
+#        if pd.isna(text):
+#            return ""
+#        matches = self.matcher.match(
+#            text.strip(),
+#            best_match=True,
+#            ignore_syntax=False,
+#        )
+#        return ", ".join([match[0]["term"] for match in matches])
+
 class KeywordExtractor:
     def __init__(self):
-        self.matcher = QuickUMLS(quickumls_fp=os.getenv("QUICKUMLS_PATH"))
+        self.matcher = Matcher()
 
     def extract_keywords(self, text: str) -> str:
         if pd.isna(text):
             return ""
-        matches = self.matcher.match(
+        matches = self.matcher.extract(
             text.strip(),
             best_match=True,
             ignore_syntax=False,
         )
-        return ", ".join([match[0]["term"] for match in matches])
-
+        return ", ".join([match["match"] for match in matches])
 
 class DataProcessor:
     def __init__(
         self,
+        note_path: str,
         sample_size: int = SAMPLE_SIZE,
         random_seed: int = RANDOM_SEED,
         seed_size: int = SEED_SIZE,
@@ -51,60 +67,56 @@ class DataProcessor:
         random.seed(random_seed)
         self.extractor = KeywordExtractor()
 
-    def load_and_sample_dataset(self) -> List[dict]:
-        dataset = load_dataset("lavita/ChatDoctor-HealthCareMagic-100k")
-        return random.sample(list(dataset["train"]), self.sample_size)
+        self.note_path = note_path
 
-    @staticmethod
-    def reformat_data_sample(sample: dict) -> dict:
-        if "input" in sample and sample["input"]:
-            sample["instruction"] = f"{sample['instruction']} {sample['input']}"
-            del sample["input"]
-        if "output" in sample:
-            sample["response"] = sample["output"]
-            del sample["output"]
-        return sample
+    def load_and_sample_dataset(self):
+        df = pd.read_csv(self.note_path)  # Adjust path here
+        sampled_df = df.sample(n=1000, random_state=42)        
+        return sampled_df
+
+    #@staticmethod
+    #def reformat_data_sample(sample: dict) -> dict:
+    #    if "input" in sample and sample["input"]:
+    #        sample["instruction"] = f"{sample['instruction']} {sample['input']}"
+    #        del sample["input"]
+    #    if "output" in sample:
+    #        sample["response"] = sample["output"]
+    #        del sample["output"]
+    #    return sample
 
     def process_data(
         self,
         seed_output_path: str = f"{OUTPUT_PATH}/private_seed.parquet",
         gen_output_path: str = f"{OUTPUT_PATH}/private.parquet",
     ):
-        sampled_data = [
-            self.reformat_data_sample(sample) for sample in self.load_and_sample_dataset()
-        ]
-        df = pd.DataFrame(sampled_data)
-        df["instruction"] = df["instruction"].apply(
-            lambda x: x.removeprefix(
-                "If you are a doctor, please answer the medical questions based on "
-                "the patient's description."
-            )
-        )
-        df["instruction_keywords"] = df["instruction"].progress_apply(
-            self.extractor.extract_keywords
-        )
-        df["response_keywords"] = df["response"].progress_apply(self.extractor.extract_keywords)
-        seed_df = self.process_dataset(df[: self.seed_size].copy())
-        gen_df = self.process_dataset(df[self.seed_size :].copy())
+        
+        df = self.load_and_sample_dataset()
+        prompt = open(PROMPT_PATH).read()
+
+        df["keywords"] = df["text"].apply(lambda x: self.extractor.extract_keywords(x))
+        df["instruction"] = df["keywords"].apply(lambda kws: prompt.replace("{keywords}", kws)).replace("{knowledge_base}", '')
+        
+        seed_df = df[: self.seed_size].copy()
+        gen_df = df[self.seed_size :].copy()
 
         for path in [seed_output_path, gen_output_path]:
             os.makedirs(os.path.dirname(path), exist_ok=True)
         seed_df.to_parquet(seed_output_path, index=False)
         gen_df.to_parquet(gen_output_path, index=False)
 
-    @staticmethod
-    def process_dataset(df: pd.DataFrame) -> pd.DataFrame:
-        prompt = open(PROMPT_PATH).read()
-        df["new_instruction"] = df.apply(
-            lambda row: prompt.replace("{keywords}", str(row["instruction_keywords"])).replace(
-                "{knowledge_base}", str(row["response_keywords"])
-            ),
-            axis=1,
-        )
-        df["output"] = df["response"]
-        df["response"] = df["instruction"]
-        df["instruction"] = df["new_instruction"]
-        return df[["instruction", "response", "output"]]
+    #@staticmethod
+    #def process_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    #    prompt = open(PROMPT_PATH).read()
+    #    df["new_instruction"] = df.apply(
+    #        lambda row: prompt.replace("{keywords}", str(row["instruction_keywords"])).replace(
+    #            "{knowledge_base}", str(row["response_keywords"])
+    #        ),
+    #        axis=1,
+    #    )
+    #    df["output"] = df["response"]
+    #    df["response"] = df["instruction"]
+    #    df["instruction"] = df["new_instruction"]
+    #    return df[["instruction", "response", "output"]]
 
 
 def generate_public_seed(
