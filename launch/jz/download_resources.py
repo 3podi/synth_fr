d8730ebdbd8e398e29a008e7362269334bcf9fbe
rflicoteaux <remi.flicoteaux@aphp.fr>
2025-06-12 14:34:52 +0000

    Avoiding Alpaca for torch safety vulnerability

--- launch/jz/download_resources.py
+++ launch/jz/download_resources.py
@@ -4,8 +4,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 AutoModelForCausalLM.from_pretrained("meta-llama/llama-2-7b-hf")
 AutoTokenizer.from_pretrained("meta-llama/llama-2-7b-hf")
 
-AutoModelForCausalLM.from_pretrained("xz97/AlpaCare-llama2-13b")
-AutoTokenizer.from_pretrained("xz97/AlpaCare-llama2-13b")
+#AutoModelForCausalLM.from_pretrained("xz97/AlpaCare-llama2-13b", use_safetensors=True)
+#AutoTokenizer.from_pretrained("xz97/AlpaCare-llama2-13b")
 
 SentenceTransformer("FremyCompany/BioLORD-2023")
 AutoTokenizer.from_pretrained("FremyCompany/BioLORD-2023")
+
+print("DOWNLOADED!")
