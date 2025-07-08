eac32837c5daa7db76b6700a49a90c791f0943b9
rflicoteaux <remi.flicoteaux@aphp.fr>
2025-07-08 15:56:05 +0000

    Added rewards grpo and grpo training script

---
+++ training_steps/alignment/grpo.py
@@ -0,0 +1,92 @@
+from unsloth import FastLanguageModel, is_bfloat16_supported
+import torch
+from trl import GRPOConfig, GRPOTrainer
+import hydra
+from training_steps.alignment.utils_grpo import format_grpo, reward_match_format_exactly, reward_f1, reward_no_repeat, reward_matching_keywords
+
+
+@hydra.main(version_base=None, config_path="./configs", config_name="grpo")
+def main(cfg):
+    """Train the model using the reinforcement learning algorithm GRPO.
+
+    Args:
+        cfg: The configuration for the training.
+    """
+    max_seq_length = 2048 # Can increase for longer reasoning traces
+    lora_rank = 64 # Larger rank = smarter, but slower
+
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = "Qwen/Qwen2.5-3B-Instruct",
+        max_seq_length = max_seq_length, #shouldnt be important if i aim at generating shorter reports
+        load_in_4bit = False, # False for LoRA 16bit
+        fast_inference = True, # Enable vLLM fast inference
+        max_lora_rank = lora_rank,
+        gpu_memory_utilization = 0.7, # Reduce if out of memory
+    )
+
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules = [
+            "q_proj", "k_proj", "v_proj", "o_proj",
+            "gate_proj", "up_proj", "down_proj",
+        ], # Remove QKVO if out of memory
+        lora_alpha = lora_rank,
+        use_gradient_checkpointing = "unsloth", # Enable long context finetuning
+        random_state = 3407,
+    )
+    
+    training_args = GRPOConfig(
+        use_vllm = True, # use vLLM for fast inference!
+        learning_rate = 5e-6,
+        adam_beta1 = 0.9,
+        adam_beta2 = 0.99,
+        weight_decay = 0.1,
+        warmup_ratio = 0.1,
+        lr_scheduler_type = "cosine",
+        optim = "adamw_8bit",
+        logging_steps = 1,
+        per_device_train_batch_size = 1,
+        gradient_accumulation_steps = 4, # Increase to 4 for smoother training
+        num_generations = 8, # Decrease if out of memory
+        max_prompt_length = 256, #### prompt gets truncated from left
+        max_completion_length = 200, ### max length of the completion, should set based on number of tokens in reports (or the length of reports i want)
+        # num_train_epochs = 1, # Set to 1 for a full training run
+        max_steps = 250,
+        save_steps = 250,
+        max_grad_norm = 0.1,
+        report_to = "none", # Can use Weights & Biases
+        output_dir = "outputs",
+    )
+
+    #### DATA ####
+    #can make a parquet from extraction results file, only need a map code2integer_id
+    dataset = Dataset.from_parquet(cfg.dataset).to_pandas().head(cfg.dataset_size)
+    dataset = dataset.map(format_grpo,batched=False)
+    
+    split_dataset = dataset.train_test_split(test_size=0.1, seed=42)
+    del dataset
+    train_dataset = split_dataset['train']
+    val_dataset = split_dataset['test']
+    del split_dataset
+    print('Len train dataset: ', len(train_dataset))
+    print('Len val dataset: ', len(val_dataset))
+    
+    #### REWARD ####
+    
+    trainer = GRPOTrainer(
+        model = model,
+        processing_class = tokenizer,
+        reward_funcs = [
+            reward_match_format_exactly, 
+            reward_f1, 
+            reward_no_repeat, 
+            reward_matching_keywords
+        ],
+        args = training_args,
+        train_dataset = dataset,
+    )
+    trainer.train()
+    
+if __name__ == "__main__":
+    main()
---
+++ training_steps/alignment/utils_grpo.py
@@ -0,0 +1,158 @@
+import re
+from collections import Counter
+
+from typing import List
+
+reasoning_start = "<réfléchir>"
+reasoning_end = "</réfléchir>"
+solution_start = "<SOLUTION>"
+solution_end = "</SOLUTION>"
+
+SYSTEM_PROMPT = f"""Vous êtes un médecin hautement spécialisé. Votre tâche consiste à rédiger un rapport d'hospitalisation fictif complet et réaliste. Le document doit présenter une évolution clinique cohérente avec une terminologie médicale précise, tout en respectant scrupuleusement la séquence imposée des mots-clés. Retournez uniquement le rapport entre {reasoning_start} et {reasoning_end}. Donnez ensuite la séquence des codes icd-10 associés à le rapport sous la forme d'une séquence de nombres entiers et placez-les entre  {solution_start} et  {solution_end}."""
+
+def remove_accents(text):
+    """Remove accents and special characters from Unicode text."""
+    return unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode('utf-8')
+
+def normalize_text(text):
+
+    # Normalize common Unicode dashes to hyphen
+    text = text.replace('\u2013', '-').replace('\u2014', '-').replace('\u2212', '-')   
+
+    # Replace hyphens with spaces
+    text = text.replace('-', ' ')
+    
+    # Remove accents
+    text = remove_accents(text)
+    
+    # Remove invisible/non-printable characters
+    text = ''.join(c for c in text if c.isprintable())
+    
+    # Remove numbers
+    text = re.sub(r'\d+', '', text)
+
+    # Replace all punctuation with whitespace
+    text = re.sub(f"[{re.escape(string.punctuation)}]", " ", text)
+    
+    # Lowercase
+    text = text.lower()
+    
+    return re.sub(r'\s+', ' ', text).strip()
+
+def format_grpo(example):
+    
+    return {
+        "prompt" : [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user",   "content": example["keywords"]},
+        ],
+        "answer": example["solution"],
+    }
+
+# Optional EOS token after </SOLUTION>
+#solution_end_regex = r"</SOLUTION>\s*(?:" + re.escape(tokenizer.eos_token) + ")?"
+solution_end_regex = r"</SOLUTION>"
+
+# Match only whitespace-separated integers between <SOLUTION> and </SOLUTION>
+integer_seq_pattern = r"(?:\d+\s*)+"
+
+match_format = re.compile(
+    rf"{reasoning_end}.*?"                        # Match reasoning end to solution start
+    rf"{solution_start}"                          # Match <SOLUTION>
+    rf"({integer_seq_pattern})"                   # Capture only integer sequence
+    rf"{solution_end_regex}",                      # Match </SOLUTION> + optional EOS
+    #rf"\s*$",                                     # Optional trailing whitespace till end of string
+    flags=re.MULTILINE | re.DO#TALL
+)
+
+match_text = re.compile(
+    re.escape(reasoning_start) + r"(.*?)" + re.escape(reasoning_end),
+    re.DOTALL
+)
+
+def reward_match_format_exactly(completions, **kwargs):
+    scores = []
+    for completion in completions:
+        score = 0
+        response = completion[0]["content"]
+        # Match if format is seen exactly!
+        if match_format.search(response) is not None: score += 3.0
+        scores.append(score)
+    return scores
+
+def count_repeats_penalty(pred_codes: List[str]):
+    counts = Counter(pred_codes)
+    penalty = sum(1 for freq in counts.values() if freq > 1)
+    reward = -1 * penalty
+    return reward
+
+def compute_f1(pred_codes: List[str], ref_codes: List[str]):
+    common = set(pred_codes) & set(ref_codes)
+    num_same = len(common)
+
+    if num_same == 0:
+        return 0.0
+
+    precision = num_same / len(pred_codes)
+    recall = num_same / len(ref_codes)
+    f1 = 2 * precision * recall / (precision + recall)
+    return f1
+
+def reward_f1(completions, answer, **kwargs):
+    
+    responses = [completion[0]["content"] for completion in completions]
+    extracted_responses = [
+        guess.group(1)
+        if (guess := match_format.search(r)) is not None else None \
+        for r in responses
+    ]
+    
+    scores = []   
+    for guess, true_answer in zip(extracted_responses, answer):
+        if guess is None:
+            continue
+        scores.append(compute_f1(guess.split(), true_answer.split()))
+    
+    return scores
+
+def reward_no_repeat(completions, **kwargs):
+    responses = [completion[0]["content"] for completion in completions]
+    extracted_responses = [
+        guess.group(1)
+        if (guess := match_format.search(r)) is not None else None \
+        for r in responses
+    ]
+    
+    scores = []   
+    for guess in extracted_responses:
+        if guess is None:
+            continue
+        
+        scores.append(count_repeats_penalty(guess.split()))
+    
+    return scores
+
+### CAREFULL: the keywords must be not normalized -> redo processing (or normalize text) (carefull partages keywords are not normalized)
+def reward_matching_keywords(prompts, completions, **kwargs):
+    question = prompts[0][-1]["content"]
+    responses = [completion[0]["content"] for completion in completions]
+
+    prompt_keywords = question.split("###")[1].split(",")
+    prompt_keywords = [kw.strip() for kw in prompt_keywords]
+    
+    extracted_texts = [
+        normalize_text(guess.group(1))
+        if (guess := match_text.search(r)) is not None else None \
+        for r in responses
+    ]
+    
+    scores[]
+    for text in extracted_texts:       
+        if text is None:
+            scores.append(len(prompt_keywords)*-1)
+            continue
+        scores.append(sum([1 for key in prompt_keywords if key in text]))
+    return scores
+
+### TODO: balance right completion score and right answer score, getting the right answer is a lot more diffucult so should deserve a lot higher score
+### TODO: answer length reward, do i need to tokenize again? 
