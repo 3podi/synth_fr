a58f45461b0c4e19e7a730f5ddaf4fada72dc2c1
3podi <riccardo1.tripodi@mail.polimi.it>
2025-10-03 18:26:43 +0200

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import argparse
+from datasets import load_dataset
 
 def setup_dataset_repo(model_name, private_seed_path, public_seed_path, dataset_size):
     """
@@ -13,7 +14,7 @@ def setup_dataset_repo(model_name, private_seed_path, public_seed_path, dataset_
     os.makedirs(base_dir, exist_ok=True)
 
     # Dataset folder name
-    folder_name = f"model={model_name.replace("/","-")}_size={dataset_size}"
+    folder_name = f"model={model_name.replace('/', '-')}_size={dataset_size}"
     dataset_dir = os.path.join(base_dir, folder_name)
     os.makedirs(dataset_dir, exist_ok=True)
 
@@ -28,6 +29,16 @@ def setup_dataset_repo(model_name, private_seed_path, public_seed_path, dataset_
     print(f"Private seed copied to: {private_dest}")
     print(f"Public seed copied to: {public_dest}")
 
+def download_hf_datasets():
+    """
+    Downloads specific Hugging Face datasets into the current directory.
+    """
+    hf_datasets = ["3podi/coded_complete", "3podi/cim_synonymes"]
+    
+    for ds_name in hf_datasets:
+        print(f"Downloading {ds_name}...")
+        dataset = load_dataset(ds_name, cache_dir=os.getcwd())
+        print(f"{ds_name} downloaded to current directory.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Setup dataset repo for synth_fr")
@@ -35,7 +46,12 @@ if __name__ == "__main__":
     parser.add_argument("--private_seed", type=str, required=True, help="Path to private seed parquet file")
     parser.add_argument("--public_seed", type=str, required=True, help="Path to public seed parquet file")
     parser.add_argument("--dataset_size", type=int, required=True, help="Dataset size to include in folder name")
+    parser.add_argument("--download_hf", action="store_false", help="Download HF files to current directory")
 
     args = parser.parse_args()
 
     setup_dataset_repo(args.model_name, args.private_seed, args.public_seed, args.dataset_size)
+
+    if args.download_hf:
+        download_hf_datasets()
+
